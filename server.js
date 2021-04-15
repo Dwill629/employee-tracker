d9b27e106d75768753ef676fde4197590bd61c49
Devon <Dwill629@gmail.com>
2021-04-15 00:31:52 -0400

    updated title on server.js

--- server.js
+++ server.js
@@ -3,3 +3,31 @@ const chalk = require("chalk");
 const figlet = require("figlet");
 const connection = require("./db.js");
 const { printTable } = require("console-table-printer");
+
+const init = () => {
+  connection.connect((err) => {
+    if (err) throw err;
+
+    console.log(
+      chalk.yellowBright(
+        "===================================================================================================="
+      )
+    );
+
+    console.log(chalk.redBright(figlet.textSync("Employee Tracker")));
+
+    console.log(
+      chalk.blueBright(
+        "                                         Created By: Devon Williams"
+      )
+    );
+
+    console.log(
+      chalk.yellowBright(
+        "===================================================================================================="
+      )
+    );
+
+    mainMenu();
+  });
+};
